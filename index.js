f2436b50cbfa0ec260ea24c0f62039d8b5845ae6
lasithLRI <lasith.hettiarachchi.dev@gmail.com>
2025-05-23 18:39:50 +0530

    - required libraries added. - main configs in index.js added - libraries added : dotenv, express, nodemon(for dev works)

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const serverPort = process.env.SERVER_PORT;
+
+
+
+app.listen(serverPort=>{
+    console.log(`server started on port ${serverPort}`);
+});
+
+app.get('/test',(req,res)=>{
+    return res.json('server works');
+})
+
